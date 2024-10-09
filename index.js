9c3bba5ed43f01ed5a13f69d9afc4eb71214a8b8
Bao44 <tqbao44@gmail.com>
2024-10-09 10:19:50 +0700

    add project xucxac

---
+++ index.js
@@ -0,0 +1,19 @@
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+  var randomImg1 = "/images/dice" + randomNumber1 + ".png";
+  var randomImg2 = "/images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img1").setAttribute("src", randomImg1);
+  document.querySelector(".img2").setAttribute("src", randomImg2);
+
+  if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+  } else {
+    document.querySelector("h1").textContent = "Draw!";
+  }
+});
